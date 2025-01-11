33c69fb02b71e892480f6d3977871c78c8a18c1f
lexYus <yusupov-alex.i@yandex.ru>
2025-01-12 00:18:05 +0500

    Добавил валидацию данных модели

--- taco-cloud/src/main/java/sia/taco_cloud/Taco.java
+++ taco-cloud/src/main/java/sia/taco_cloud/Taco.java
@@ -1,11 +1,19 @@
 package sia.taco_cloud;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NonNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
