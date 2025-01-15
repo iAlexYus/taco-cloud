81cd73125719150e75531790f0238e5b2c1741ff
lexYus <yusupov-alex.i@yandex.ru>
2025-01-15 23:01:42 +0500

    Добавил зависимость Jdbc и H2, заменил файл .properties на .yml, добавил репозиторий для работы с данными в БД для Ingredient

---
+++ taco-cloud/src/main/java/sia/taco_cloud/data/IngredientRepository.java
@@ -0,0 +1,14 @@
+package sia.taco_cloud.data;
+
+import sia.taco_cloud.models.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
--- taco-cloud/src/main/java/sia/taco_cloud/models/Ingredient.java
+++ taco-cloud/src/main/java/sia/taco_cloud/models/Ingredient.java
@@ -1,14 +1,8 @@
 package sia.taco_cloud.models;
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-//@Getter
-//@Setter
-//@Builder
 public class Ingredient {
     private final String id;
     private final String name;
