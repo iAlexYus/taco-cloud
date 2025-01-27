e213f3293bb4c59a8ab7e85af60b22bc3c45b703
lexYus <yusupov-alex.i@yandex.ru>
2025-01-27 21:16:08 +0500

    Добавил конфигурацию для работы с авторизацией пользователя с нужными условиями, добавил простую конфигурацию контроллера для авторизации и страницу авторизации

--- taco-cloud/src/main/java/sia/taco_cloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/sia/taco_cloud/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package sia.taco_cloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import sia.taco_cloud.data.UserRepository;
 import sia.taco_cloud.models.User;
 
@@ -41,6 +43,20 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User" + username + " not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                //можно указывать разные настройки поведения
+                .defaultSuccessUrl("/design")
+                .and()
+                .build();
     }
 
 }
